26f943e8a2d954f68fd7287abb2188b460499b62
Ryan Gosling <shakeveetof@gmail.com>
2026-06-05 21:06:43 +0300

    task created

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
-const input = document.querySelector('.text__add');
-const header__btn = document.querySelector('.header__btn');
+const input = document.querySelector(".text__add");
+const header__btn = document.querySelector(".header__btn");
+const addButton = document.querySelector(".header__btn");
+const taskContainer = document.querySelector(".tasks");
 
-header__btn.addEventListener('click',function(){
-    console.log(input.value);
+const tasks = document.createElement("div");
+tasks.classList.add("task");
+
+header__btn.addEventListener("click", function () {
+  const task = document.createElement("div");
+  task.classList.add("task");
+
+  const checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  const editBtn = document.createElement("button");
+  editBtn.textContent = "edit";
+
+  const title = document.createElement("span");
+  title.textContent = input.value;
+  const deleteBtn = document.createElement("button");
+  deleteBtn.textContent = "delete";
+  taskContainer.append(task);
+  task.append(checkbox);
+  task.append(title);
+  task.append(editBtn);
+  task.append(deleteBtn);
 });
