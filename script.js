a2dfacad53eccf4e7ad4137d9531c15ff2cba089
Ryan Gosling <shakeveetof@gmail.com>
2026-06-10 17:15:29 +0300

    added delete button and fixed empty task

--- script.js
+++ script.js
@@ -1,27 +1,32 @@
-const input = document.querySelector(".text__add");
-const header__btn = document.querySelector(".header__btn");
+const inputTask = document.querySelector(".text__add");
+const headerButton = document.querySelector(".header__btn");
 const addButton = document.querySelector(".header__btn");
 const taskContainer = document.querySelector(".tasks");
 
-const tasks = document.createElement("div");
-tasks.classList.add("task");
+headerButton.addEventListener("click", function () {
+  if (inputTask.value === "") {
+    return;
+  }
 
-header__btn.addEventListener("click", function () {
   const task = document.createElement("div");
   task.classList.add("task");
 
-  const checkbox = document.createElement("input");
+  const checkbox = document.createElement("inputTask");
   checkbox.type = "checkbox";
   const editBtn = document.createElement("button");
   editBtn.textContent = "edit";
 
   const title = document.createElement("span");
-  title.textContent = input.value;
+  title.textContent = inputTask.value;
   const deleteBtn = document.createElement("button");
   deleteBtn.textContent = "delete";
-  taskContainer.append(task);
+  deleteBtn.addEventListener("click", function () {
+    task.remove();
+  });
+
   task.append(checkbox);
   task.append(title);
   task.append(editBtn);
   task.append(deleteBtn);
+  taskContainer.append(task);
 });
